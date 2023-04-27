53c04439ed301114c9e389d7f1f5d41fc3f7148f
SaiHlyanPhyoe <saihlyan.phyoe@kbzbank.com>
2023-04-27 09:30:08 +0630

    classvalidator complete

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,18 +1,40 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  IsOptional,
+  IsBoolean,
+} from 'class-validator';
 
 export class CreateArticleDto {
+  @IsNotEmpty()
+  @MinLength(5)
+  @IsString()
   @ApiProperty()
   title: string;
 
+  @IsNotEmpty()
+  @MinLength(5)
+  @IsString()
   @ApiProperty()
   author: string;
 
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  @MaxLength(300)
   @ApiProperty({ required: false })
   description?: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   body: string;
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   published?: boolean = false;
 }
